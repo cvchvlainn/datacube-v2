616aff46a7f050856cce24b5ef774aa140cdf0e8
cvchvlainn <128788769+cvchvlainn@users.noreply.github.com>
2024-06-10 02:04:10 +0600

    Add files via upload

---
+++ blocked.php
@@ -0,0 +1,14 @@
+<?php 
+   require_once './app/components/header.php';
+   require_once './app/action/dataOrdering/organizingBlockedData.php';
+?>
+   <main>
+      <div class="main-container">
+         <div class="main-blocked">
+            Вы заблокированы
+         </div>
+      </div>
+   </main>
+   <script src="./assets/scripts/header.js"></script>
+</body>
+</html>
